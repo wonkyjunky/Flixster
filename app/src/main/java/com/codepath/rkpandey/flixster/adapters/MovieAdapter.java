55a8fbf3718b0795ab980f9955f7379723b53e2f
wonkyjunky <wjeong@csumb.edu>
2020-02-19 20:35:57 -0800

    adding unit 2 features

--- app/src/main/java/com/codepath/rkpandey/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/rkpandey/flixster/adapters/MovieAdapter.java
@@ -1,12 +1,14 @@
 package com.codepath.rkpandey.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,8 +16,11 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.codepath.rkpandey.flixster.DetailAcitivity;
 import com.codepath.rkpandey.flixster.R;
 import com.codepath.rkpandey.flixster.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -51,6 +56,8 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -60,6 +67,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
         public void bind(final Movie movie) {
@@ -75,11 +83,14 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             Glide.with(context).load(imageUrl).into(ivPoster);
 
             //1.Register click listener on the whole row
-            //2. Navigate to a new activity on tap
-            tvTitle.setOnClickListener(new View.OnClickListener() {
+            container.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(context, movie.getTitle(),Toast.LENGTH_SHORT).show();
+                    //2. Navigate to a new activity on tap
+                    Intent i = new Intent(context, DetailAcitivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+//                    Toast.makeText(context, movie.getTitle(),Toast.LENGTH_SHORT).show();
                 }
             });
         }
